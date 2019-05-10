6b23849b490d4d0036b637e761cdb7479da5b2b1
maruta atsuki <s1711567@s.tsukuba.ac.jp>
2019-05-10 16:13:46 +0900

    db

--- db/migrate/20190510063756_create_eateries.rb
+++ db/migrate/20190510063756_create_eateries.rb
@@ -1,8 +1,9 @@
 class CreateEateries < ActiveRecord::Migration[5.2]
   def change
     create_table :eateries do |t|
-      t.string, :name
-      t.string,address :zip
+      t.string :name
+      t.string :zip
+      t.string :address
       t.string :tel
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Eatery.create(name: "te.to.te", zip:"305-0003",
+   address:"茨城県つくば市桜２丁目１−７ グランパスつくば102号", tel:"029-857-7793")
