5f2b08c78d4cd3bd182f8a4d94e2aec0851a153a
maruta atsuki <s1711567@s.tsukuba.ac.jp>
2019-05-17 16:08:59 +0900

    フォーム

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'lunch/go'
   root 'lunch#go'
+  post 'eateries/create'
 end
